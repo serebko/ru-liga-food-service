3ccb0f891da1ba333e82ea34aac0adfe3c021e79
Anton Serebrennikov <serebko@yandex.ru>
2023-10-08 14:15:52 +0300

    Create a liga_education project

---
+++ src/main/java/ru/liga/liga_education/LigaEducationApplication.java
@@ -0,0 +1,13 @@
+package ru.liga.liga_education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigaEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/liga/liga_education/LigaEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liga.liga_education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
