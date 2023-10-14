e1b3e834959cfb8cd86fa634d3b1d447ad1c03b5
Anton Serebrennikov <serebko@yandex.ru>
2023-10-15 00:18:20 +0300

    Create db scripts
    Add init.sql and init_rollback.sql

--- migration/src/main/resources/db/changelog/v01/init.sql
+++ migration/src/main/resources/db/changelog/v01/init.sql
@@ -1,39 +1,127 @@
-create sequence if not exists pib_right_seq;
+create sequence if not exists couriers_seq;
 
-create table if not exists pib_right
+create table if not exists couriers
 (
-    right_id bigint not null default nextval('pib_right_seq'),
-    rights jsonb,
-    create_dttm timestamptz  not null default now(),
-    modify_dttm timestamptz  not null default now(),
-    constraint pib_right_pk primary key (right_id)
-    );
+    id bigint not null default nextval('couriers_seq'),
+    phone varchar(20) not null,
+    status varchar(10),
+    coordinates varchar(30),
+    constraint couriers_pk primary key (id)
+);
 
-comment on table pib_right is 'Права всех пользователей';
-comment on column pib_right.right_id is 'Идентификатор';
-comment on column pib_right.rights is 'Права';
-comment on column pib_right.create_dttm is 'Дата время вставки записи в таблицу';
-comment on column pib_right.modify_dttm is 'Дата время последнего изменения записи';
+comment on table couriers is 'Доставки';
+comment on column couriers.id is 'Идентификатор курьера';
+comment on column couriers.phone is 'Номер телефона курьера';
+comment on column couriers.status is 'Статус доставки';
+comment on column couriers.coordinates is 'Координаты курьера';
 
-create sequence if not exists pib_user_seq;
+create sequence if not exists customers_seq;
 
-create table if not exists pib_user
+create table if not exists customers
 (
-    user_id bigint not null default nextval ('pib_user_seq'),
-    physical_person_id bigint,
-    right_id bigint,
-    login varchar(255),
-    create_dttm timestamptz  not null default now(),
-    modify_dttm timestamptz  not null default now(),
-    constraint pib_user_pk primary key (user_id),
-    constraint pib_user_pib_right_fk foreign key (right_id)
-    references pib_right (right_id)
-    );
+    id bigint not null default nextval('customers_seq'),
+    phone varchar(20) not null,
+    email varchar(255),
+    address varchar(255) not null,
+    constraint customers_pk primary key (id)
+);
 
-comment on table pib_user is 'Пользователи';
-comment on column pib_user.user_id is 'Идентификатор пользователя';
-comment on column pib_user.physical_person_id is 'Идентификатор физ лица';
-comment on column pib_user.right_id is 'Идентификатор прав';
-comment on column pib_user.login is 'Логин';
-comment on column pib_user.create_dttm is 'Дата время вставки записи в таблицу';
-comment on column pib_user.modify_dttm is 'Дата время последнего изменения записи';
+comment on table customers is 'Заказчики';
+comment on column customers.id is 'Идентификатор заказчика';
+comment on column customers.phone is 'Номер телефона заказчика';
+comment on column customers.email is 'Email заказчика';
+comment on column customers.address is 'Адрес заказчика';
+
+create sequence if not exists restaurants_seq;
+
+create table if not exists restaurants
+(
+    id bigint not null default nextval('restaurants_seq'),
+    address varchar(255) not null,
+    status varchar(10),
+    constraint restaurant_pk primary key (id)
+);
+
+comment on table restaurants is 'Рестораны';
+comment on column restaurants.id is 'Идентификатор ресторана';
+comment on column restaurants.address is 'Адрес ресторана';
+comment on column restaurants.status is 'Статус кухни';
+
+create sequence if not exists orders_seq;
+
+create table if not exists orders
+(
+    id bigint not null default nextval('orders_seq'),
+    customer_id bigint not null,
+    restaurant_id bigint not null,
+    status varchar(10),
+    courier_id bigint not null,
+    timestamp timestamptz not null default now(),
+    constraint orders_pk primary key (id),
+    constraint orders_customers_fk foreign key (customer_id)
+    references customers (id),
+    constraint orders_restaurants_fk foreign key (restaurant_id)
+    references restaurants (id),
+    constraint orders_couriers_fk foreign key (courier_id)
+    references couriers (id)
+);
+
+comment on table orders is 'Заказы';
+comment on column orders.id is 'Идентификатор заказа';
+comment on column orders.customer_id is 'Идентификатор заказчика';
+comment on column orders.restaurant_id is 'Идентификатор ресорана';
+comment on column orders.status is 'Статус заказа';
+comment on column orders.courier_id is 'Идентификатор курьера';
+comment on column orders.timestamp is 'Дата время размещения заказа';
+
+create sequence if not exists restaurant_menu_items_seq;
+
+create table if not exists restaurant_menu_items
+(
+    id bigint not null default nextval('restaurant_menu_items_seq'),
+    restaurant_id bigint not null,
+    name varchar(255) not null unique,
+    price money not null default 0.00,
+    image text,
+    description varchar(255),
+    constraint restaurant_menu_items_pk primary key (id),
+    constraint restaurant_menu_items_restaurants_fk
+    foreign key (restaurant_id) references restaurants (id)
+);
+
+comment on table restaurant_menu_items is 'Позиции в меню ресторана';
+comment on column restaurant_menu_items.id is 'Идентификатор позиции меню';
+comment on column restaurant_menu_items.restaurant_id is 'Идентификатор ресторана';
+comment on column restaurant_menu_items.name is 'Имя позиции';
+comment on column restaurant_menu_items.price is 'Цена позиции';
+comment on column restaurant_menu_items.image is 'Изображение позиции';
+comment on column restaurant_menu_items.description is 'Описание позиции';
+
+
+create sequence if not exists order_items_seq;
+
+create table if not exists order_items
+(
+    id bigint not null default nextval('order_items_seq'),
+    order_id bigint not null,
+    restaurant_menu_item bigint not null,
+    price money not null default 0.00,
+    quantity integer not null default 0,
+    constraint order_items_pk primary key (id),
+    constraint order_items_restaurant_menu_items_fk
+    foreign key (restaurant_menu_item)
+    references restaurant_menu_items (id)
+);
+
+comment on table order_items is 'Позиции заказа';
+comment on column order_items.id is 'Идентификатор позиции заказа';
+comment on column order_items.order_id is 'Идентификатор заказа';
+comment on column order_items.restaurant_menu_item is 'Позиция из меню ресторана';
+comment on column order_items.price is 'Общая цена позиции';
+comment on column order_items.quantity is 'Количество товаров в позиции';
+
+
+
+
+
+
